a3334763e6700224bbfdda71faab355de2287425
wugui2020 <momi2020@uw.edu>
2016-04-06 14:25:28 -0400

    Credential and cfg info initialization

--- quickstart.py
+++ quickstart.py
@@ -16,8 +16,16 @@ except ImportError:
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/drive-python-quickstart.json
 SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly'
-CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'Drive API Python Quickstart'
+CLIENT_SECRET_FILE = 'client_secret_426774337873-0jm29chokpdr9pm0scnla58ck6g1s0r3.apps.googleusercontent.com.json'
+APPLICATION_NAME = 'Wdrive'
+
+
+def get_path():
+    home_dir = os.path.expanduser('~')
+    profile_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(profile_dir):
+        os.makedirs(profile_dir)
+    return profile_dir
 
 
 def get_credentials():
@@ -29,12 +37,9 @@ def get_credentials():
     Returns:
         Credentials, the obtained credential.
     """
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
+    credential_dir = get_path()
     credential_path = os.path.join(credential_dir,
-                                   'drive-python-quickstart.json')
+                                   'Wdrive.json')
 
     store = oauth2client.file.Storage(credential_path)
     credentials = store.get()
@@ -48,25 +53,52 @@ def get_credentials():
         print('Storing credentials to ' + credential_path)
     return credentials
 
-def main():
-    """Shows basic usage of the Google Drive API.
+def get_configs():
+    config_dir = get_path()
+    config_path = os.path.join(config_dir,
+                                   'Wdrive.cfg')
+    try:
+        config = open(config_path, 'r')
+        OPTOUTLIST = config.readline().split(',')
+    except IOError:
+        config = open(config_path, 'w')
+        OPTOUTLIST = []
+        config.write(",".join(map(str,OPTOUTLIST)))
+        print ("New profile established. Please use\n\t\twdrive ignore\nto opt out folders you don't want to be synced.")
+    
+    config.close()
 
-    Creates a Google Drive API service object and outputs the names and IDs
-    for up to 10 files.
-    """
+    return OPTOUTLIST
+
+
+def initialze():
+    """Get the credentials and set the parameters for sync"""
+    """LOCALPATH:  (String) The directory that will be local Google Drive folder."""
+    """OPTOUTLIST: (List) The list of IDs that will not be synced with the cloud."""
+
+    LOCALPATH = os.getcwd()+"/googledrive"
+    print (LOCALPATH)
+    if not os.path.exists(LOCALPATH):
+        os.makedirs(LOCALPATH)
     credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    service = discovery.build('drive', 'v3', http=http)
+    OPTOUTLIST = get_configs()
 
-    results = service.files().list(
-        pageSize=10,fields="nextPageToken, files(id, name)").execute()
-    items = results.get('files', [])
-    if not items:
-        print('No files found.')
-    else:
-        print('Files:')
-        for item in items:
-            print('{0} ({1})'.format(item['name'], item['id']))
+    try:
+        http = credentials.authorize(httplib2.Http())
+        service = discovery.build('drive', 'v3', http=http)
+    except httplib2.ServerNotFoundError:
+        print ("ServerNotFoundError: Please try again later")
+
+def opt_out(ID):
+    config_dir = get_path()
+    config_path = os.path.join(config_dir,
+                                   'Wdrive.cfg')
+    config = open(config_path, 'r')
+    OPTOUTLIST = config.readline().split(',')
+    OPTOUTLIST.append(ID)
+    config.write(",".join(map(str,OPTOUTLIST)))
+    config.close()
+    return True
 
 if __name__ == '__main__':
-    main()
+    initialze()
